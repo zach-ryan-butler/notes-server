26d0f7f7fac03e64a69c91f757c4c9ef435bb376
Zach Butler <zach.standley93@gmail.com>
2019-09-03 10:09:20 -0700

    refactored seed data file to work with test

--- test/seedData.js
+++ test/seedData.js
@@ -2,26 +2,21 @@ const Note = require('../lib/models/Notes');
 
 const notes = [
   {
-    'title': 'Dogs',
-    'body': 'pepper, skip, john'
+    title: 'Dogs',
+    body: 'pepper, skip, john'
   },
   {
-    'title': 'Cats',
-    'body': 'joan, snowball, salt'
+    title: 'Cats',
+    body: 'joan, snowball, salt'
   },
   {
-    'title': 'Birds',
-    'body': 'tim, maxwell, lisa'
+    title: 'Birds',
+    body: 'tim, maxwell, lisa'
   },
   {
-    'title': 'cows',
-    'body': 'spot, milk, giver'
+    title: 'cows',
+    body: 'spot, milk, giver'
   }
 ];
 
-module.exports = async() => {
-  const noteSeed = await notes.map(note => {
-    return Note.create(note);
-  });
-  return noteSeed;
-};
+module.exports = () => Note.create(notes);
